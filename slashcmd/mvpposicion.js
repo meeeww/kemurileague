20a019d1cb6e89cd77af18fbfb33bd538f9ed38a
mew <102923191+meeeww@users.noreply.github.com>
2022-08-02 18:24:16 +0200

    kemurileague

--- slashcmd/mvpposicion.js
+++ slashcmd/mvpposicion.js
@@ -50,9 +50,7 @@ module.exports = {
                 listaJugadores.push([dataGotten2["values"][i + 1][3], dataGotten2["values"][i + 1][0]])
             }
         }
-        var listJugadoresSorted = listaJugadores.sort(function(a, b) {
-            return a - b;
-          });
+        var listJugadoresSorted = listaJugadores.sort();
         console.log(listJugadoresSorted)
         //console.log(parseInt(jornada) + parseInt(sumar1))
         setTimeout(async () => {
