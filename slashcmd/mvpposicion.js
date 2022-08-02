9b19189046d96aaa31e9947fa0a64bbc559c55a1
mew <102923191+meeeww@users.noreply.github.com>
2022-08-02 19:13:57 +0200

    kemurileague

--- slashcmd/mvpposicion.js
+++ slashcmd/mvpposicion.js
@@ -81,39 +81,8 @@ module.exports = {
             .setColor("#fca2ad")
             .setTitle("MVPs de "+interaction.options.getString("posicion"))
             .setDescription("De toda la comp")
-            .addFields(
-                {
-                    "name": "🥇"+listaJugadores[0][1],
-                    "value": "MVPs: "+listaJugadores[0][0],
-                    //inline: true
-                },
-                {
-                    "name": "🥈"+listaJugadores[1][1],
-                    "value": "MVPs: "+listaJugadores[1][0],
-                    //inline: true
-                },
-                {
-                    "name": "🥉"+listaJugadores[2][1],
-                    "value": "MVPs: "+listaJugadores[2][0],
-                    //inline: true
-                },
-                {
-                    "name": listaJugadores[3][1],
-                    "value": "MVPs: "+listaJugadores[3][0],
-                    //inline: true
-                },
-                {
-                    "name": listaJugadores[4][1],
-                    "value": "MVPs: "+listaJugadores[4][0],
-                    //inline: true
-                },
-                {
-                    "name": listaJugadores[5][1],
-                    "value": "MVPs: "+listaJugadores[5][0],
-                    //inline: true
-                }
-                )
-                .setFooter({text: "Kemuri League"})
+            
+            .setFooter({text: "Kemuri League"})
             //
             const fail = new Discord.EmbedBuilder()
             .setColor("#fca2ad")
