65c72f9cd6fc6b5467a45bb3f26e0ff6a2afc793
mew <102923191+meeeww@users.noreply.github.com>
2022-08-06 14:30:08 +0200

    kemurileague

--- slashcmd/mvpposicion.js
+++ slashcmd/mvpposicion.js
@@ -47,7 +47,7 @@ module.exports = {
         var listaJugadores = []
         for(var i = 0; i < dataGotten2["values"][0][39]; i++){
             if(dataGotten2["values"][i + 1][7] == interaction.options.getString("posicion")){
-                listaJugadores.push([dataGotten2["values"][i + 1][3], dataGotten2["values"][i + 1][0], dataGotten2["values"][i + 1][1]])
+                listaJugadores.push([parseInt(dataGotten2["values"][i + 1][3]), dataGotten2["values"][i + 1][0], dataGotten2["values"][i + 1][1]])
             }
         }
         
